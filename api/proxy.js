b5692592b41706e41bff57a0e49dd9c3fe8ccac8
oukhanabil2 <oukhanabil2@gmail.com>
2026-04-27 18:05:28 +0100

    Add proxy handler for API requests

---
+++ api/proxy.js
@@ -0,0 +1,7 @@
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+  res.status(200).json({ message: 'Proxy is ready. Use /api/Agents, etc.' });
+}
